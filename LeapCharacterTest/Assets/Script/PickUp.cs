172793027ef460d4e14ed7c78bdaaf9d724cec6a
IsakN <isaq.tokyo@gmail.com>
2020-03-23 15:31:38 +0100

    Mer saker tilllagt

---
+++ LeapCharacterTest/Assets/Script/PickUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform theDest;
+
+    private Rigidbody rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnMouseDown()
+    {
+        rb.isKinematic = true;
+        //rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+        this.transform.position = theDest.position;
+        this.transform.parent = GameObject.Find("PickUpDestination").transform;
+    }
+
+    void OnMouseUp()
+    {
+        this.transform.parent = null;
+        rb.useGravity = true;
+        rb.isKinematic = false;
+    }
+}
