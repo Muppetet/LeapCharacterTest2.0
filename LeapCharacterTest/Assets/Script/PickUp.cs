5cb1530599e0b203e6d538c3e94df33989d2d288
IsakN <isaq.tokyo@gmail.com>
2020-03-24 12:11:02 +0100

    MerSeeSaws

--- LeapCharacterTest/Assets/Script/PickUp.cs
+++ LeapCharacterTest/Assets/Script/PickUp.cs
@@ -13,17 +13,20 @@ public class PickUp : MonoBehaviour
     }
     void OnMouseDown()
     {
-        rb.isKinematic = true;
-        //rb.useGravity = false;
-        rb.velocity = Vector3.zero;
-        this.transform.position = theDest.position;
-        this.transform.parent = GameObject.Find("PickUpDestination").transform;
+        Vector3 dist = Camera.main.transform.position - this.transform.position;
+        if (dist.magnitude < 3)
+        {
+            rb.isKinematic = true;
+            rb.velocity = Vector3.zero;
+            this.transform.position = theDest.position;
+            this.transform.parent = GameObject.Find("PickUpDestination").transform;
+        }
+        
     }
 
     void OnMouseUp()
     {
         this.transform.parent = null;
-        rb.useGravity = true;
         rb.isKinematic = false;
     }
 }
---
+++ LeapCharacterTest/Assets/Script/RigidbodyCharacter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyCharacter : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
