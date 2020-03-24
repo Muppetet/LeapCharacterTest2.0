229326823d77b66067a743584fafb2f363125d64
IsakN <isaq.tokyo@gmail.com>
2020-03-24 20:11:12 +0100

    KnappTest

---
+++ LeapCharacterTest/Assets/PushButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushButton : MonoBehaviour
+{
+    public Transform buttonStart;
+    public Transform button;
+    public Transform spawnPoint;
+    public GameObject block;
+    bool buttonPressed;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        buttonPressed = true;
+        SpawnBlock();
+    }
+
+    private void SpawnBlock()
+    {
+        Instantiate(block, spawnPoint.position, Quaternion.identity);
+    }
+
+    private void Update()
+    {
+        if (buttonPressed)
+        {
+            if (button.localPosition.z < buttonStart.localPosition.z)
+            {
+                Debug.Log("Test");
+            }
+                
+        }
+
+    }
+}
