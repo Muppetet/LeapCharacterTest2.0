0887a4449aa6b919dbf28d8abb0414da3609c976
IsakN <isaq.tokyo@gmail.com>
2020-03-26 15:50:21 +0100

    LitemEr

--- LeapCharacterTest/Assets/Script/PickUp.cs
+++ LeapCharacterTest/Assets/Script/PickUp.cs
@@ -7,6 +7,7 @@ public class PickUp : MonoBehaviour
     public Transform theDest;
 
     private Rigidbody rb;
+    private Rigidbody prb;
     private void Awake()
     {
         theDest = GameObject.FindWithTag("PickUpDest").transform;
@@ -17,6 +18,8 @@ public class PickUp : MonoBehaviour
         Vector3 dist = Camera.main.transform.position - this.transform.position;
         if (dist.magnitude < 3)
         {
+            prb = theDest.GetComponentInParent<Rigidbody>();
+            prb.mass+=rb.mass;
             rb.isKinematic = true;
             rb.velocity = Vector3.zero;
             this.transform.position = theDest.position;
@@ -29,5 +32,6 @@ public class PickUp : MonoBehaviour
     {
         this.transform.parent = null;
         rb.isKinematic = false;
+        prb.mass -= rb.mass;
     }
 }
