20e039cb8976bfc0ad573c9f3c09528d9763ff75
IsakN <isaq.tokyo@gmail.com>
2020-03-26 15:18:20 +0100

    NågraFlerTester

---
+++ LeapCharacterTest/Assets/KeyButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyButton : MonoBehaviour
+{
+    public string tag;
+
+    public Transform spawnPoint;
+    public GameObject spawnItem;
+
+    GameObject spawnedGO;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(tag) && spawnedGO == null)
+        {
+            spawnedGO = Instantiate(spawnItem, spawnPoint.position, Quaternion.identity);
+        } else if (other.CompareTag(tag))
+        {
+            spawnedGO.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(tag))
+        {
+            spawnedGO.SetActive(false);
+        }
+    }
+}
