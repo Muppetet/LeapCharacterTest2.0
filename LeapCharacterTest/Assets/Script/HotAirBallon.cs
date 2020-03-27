e0c1222ec727998c58660f77700d29e8de2145f6
IsakN <isaq.tokyo@gmail.com>
2020-03-27 09:45:33 +0100

    Ballon?

---
+++ LeapCharacterTest/Assets/Script/HotAirBallon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotAirBallon : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forceAmount;
+    public ForceMode forceMode;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            rb.AddForce(forceAmount * Vector3.up * Time.deltaTime, forceMode);
+        }
+        
+    }
+}
