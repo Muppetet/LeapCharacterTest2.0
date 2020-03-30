fd4fa7594eb59b94aef874bb0782f133c2c927b0
IsakN <isaq.tokyo@gmail.com>
2020-03-30 15:37:48 +0200

    Fixed pickUp for CC

--- LeapCharacterTest/Assets/Script/PickUp.cs
+++ LeapCharacterTest/Assets/Script/PickUp.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class PickUp : MonoBehaviour
 {
     public Transform theDest;
-
+    
+    private bool rbCharacterController;
     private Rigidbody rb;
     private Rigidbody prb;
     private void Awake()
     {
         theDest = GameObject.FindWithTag("PickUpDest").transform;
         rb = GetComponent<Rigidbody>();
+        if (GameObject.Find("RigidBodyController"))
+            rbCharacterController = true;
     }
     void OnMouseDown()
     {
         Vector3 dist = Camera.main.transform.position - this.transform.position;
-        if (dist.magnitude < 3)
+        if (dist.magnitude < 5)
         {
             prb = theDest.GetComponentInParent<Rigidbody>();
-            prb.mass+=rb.mass;
+            if(rbCharacterController)
+                prb.mass+=rb.mass;
             rb.isKinematic = true;
             rb.velocity = Vector3.zero;
             this.transform.position = theDest.position;
@@ -32,6 +36,7 @@ public class PickUp : MonoBehaviour
     {
         this.transform.parent = null;
         rb.isKinematic = false;
-        prb.mass -= rb.mass;
+        if (rbCharacterController)
+            prb.mass -= rb.mass;
     }
 }
