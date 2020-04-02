30ed2d65c4a61887dc6979433f14477d4171330f
IsakN <isaq.tokyo@gmail.com>
2020-04-02 21:37:39 +0200

    TestPuzzle

---
+++ LeapCharacterTest/Assets/Script/DeathZone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //if(other.gameObject.CompareTag("Player"))
+            other.gameObject.transform.SetPositionAndRotation(new Vector3(0, 2, 0), Quaternion.identity);
+    }
+}
