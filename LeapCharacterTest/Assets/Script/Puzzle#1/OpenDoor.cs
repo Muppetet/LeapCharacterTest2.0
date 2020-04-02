ab2d66bcf4d827771f903072dc224b6b54ce4525
Muppetet <55240545+Muppetet@users.noreply.github.com>
2020-04-02 21:13:25 +0200

    Buildt the first puzzle

---
+++ LeapCharacterTest/Assets/Script/Puzzle#1/OpenDoor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+
+    public GameObject door;
+
+    bool isOpen = false; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isOpen && TriggerSound.StringOn && TriggerSoundBell.BellOn && TriggerSoundTrumpet.TrumpetOn)
+        {
+            door.transform.position += new Vector3(4, 0, 0);
+            isOpen = true; 
+        }
+        
+    }
+}
---
+++ LeapCharacterTest/Assets/Script/Puzzle#1/TriggerSoundTrumpet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSoundTrumpet : MonoBehaviour
+{
+    public AudioClip triggerSound;
+    AudioSource audioSource;
+    public static bool TrumpetOn = false;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (triggerSound != null)
+        {
+            audioSource.PlayOneShot(triggerSound, 0.7F);
+            TrumpetOn = true;
+        }
+    }
+}
